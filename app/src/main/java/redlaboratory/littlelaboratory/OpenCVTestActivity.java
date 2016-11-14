833200170019ab2a8863dfc06a223e4019825722
심지수 <rlj1202@gmail.com>
2016-11-14 21:20:38 +0900

    2016/11/14 @School
    OpenCV코드만 만지작 만지작
    원래 오늘 과제연구 날은 아니지만 과제연구하려고 교실에 남음.

--- app/src/main/java/redlaboratory/littlelaboratory/OpenCVTestActivity.java
+++ app/src/main/java/redlaboratory/littlelaboratory/OpenCVTestActivity.java
@@ -106,26 +106,142 @@ public class OpenCVTestActivity extends AppCompatActivity implements CameraBridg
 
     }
 
-    Mat mGrey;
-    Mat mRgba;
-
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        mGrey = inputFrame.gray();
-        mRgba = inputFrame.rgba();
+        Mat grey = inputFrame.gray();
+        Mat rgba = inputFrame.rgba();
+
+        int width = rgba.width();
+        int height = rgba.height();
+
+        int minWidth = 10;
+        int maxWidth = 1000;
+        int minHeight = 10;
+        int maxHeight = 1000;
 
         Mat binarization = new Mat();
 
-        Imgproc.threshold(mGrey, binarization, 100, 255, Imgproc.THRESH_BINARY);
+        Imgproc.threshold(grey, binarization, 127.0d, 225.0d, Imgproc.THRESH_BINARY_INV);
+
+        Rect[] rectsResult;
+        {
+            int[] imgBuffer = new int[width * height];
+            int[] labelBuffer = new int[width * height];
+
+            Rect[] rectLable = new Rect[width * height];
+
+            int labelCount = 0;
+
+            for (int h = 0; h < height; h++) {
+                for (int w = 0; w < width; w++) {
+                    imgBuffer[w + h * width] = (int) binarization.get(w, h)[0];// NULL POINTER EXCEPTION
+                    labelBuffer[w + h * width] = -1;
+                }
+            }
+
+            for (int h = 0; h < height; h++) {
+                for (int w = 0; w < width; w++) {
+                    if (imgBuffer[w + h * width] == 255 && labelBuffer[w + h * width] == -1) {
+                        Rect temp = new Rect();
+                        temp.x = w;
+                        temp.y = h;
+                        temp.width = 0;
+                        temp.height = 0;
+                        labelCount++;
+
+                        labeling(imgBuffer, labelBuffer, width, height, w, h, temp, labelCount);
+
+                        if (temp.width < minWidth || temp.width > maxWidth || temp.height < minHeight || temp.height > maxHeight) labelCount--;
+                        else rectLable[labelCount - 1] = temp;
+                    }
+                }
+            }
+
+            rectsResult = rectLable;
+        }
+
+        for (Rect rect : rectsResult) {
+            Imgproc.rectangle(rgba, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(1, 0, 0));
+        }
+
+//        Imgproc.threshold(mGrey, binarization, 100, 255, Imgproc.THRESH_BINARY);
 //        Imgproc.rectangle(mRgba, new Point(100, 200), new Point(100, 100), new Scalar(256, 0, 0), 3);
 
-        ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-        Mat intermediate = new Mat();
-        Imgproc.Canny(mRgba, intermediate, 80, 100);
-        Imgproc.findContours(intermediate, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0));
-        Imgproc.drawContours(mRgba, contours, -1, new Scalar(255, 0, 0));
+//        ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+//        Mat intermediate = new Mat();
+//        Imgproc.Canny(mRgba, intermediate, 80, 100);
+//        Imgproc.findContours(intermediate, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0));
+//        Imgproc.drawContours(mRgba, contours, -1, new Scalar(255, 0, 0));
 
-        return mRgba;
+        return rgba;
+    }
+
+    private void labeling(int[] imgBuffer, int[] labelBuffer, int width, int height, int x, int y, Rect label, int labelNum) {
+        labelBuffer[x + y * width] = labelNum;
+        Point[] stack = new Point[width * height];
+        stack[0].x = x;
+        stack[0].y = y;
+        int stackCount = 1;
+
+        while (stackCount > 0) {
+            stackCount--;
+
+            x = (int) stack[stackCount].x;
+            y = (int) stack[stackCount].y;
+
+            if (label.x > x) {
+                label.x = x;
+            } else if (label.x + label.width < x) {
+                label.width = x - label.x;
+            }
+            if (label.y > y) {
+                label.y = y;
+            } else if (label.y + label.height < y) {
+                label.height = y - label.y;
+            }
+
+            int nX = x - 1;
+            int pX = x + 1;
+            int nY = y - 1;
+            int pY = y + 1;
+
+            if (nX >= 0) {
+                if (imgBuffer[nX + y * width] == 255 & labelBuffer[nX + y * width] == -1) {
+                    stack[stackCount].x = nX;
+                    stack[stackCount].y = y;
+
+                    labelBuffer[nX + y * width] = labelNum;
+                    stackCount++;
+                }
+            }
+            if (nY >= 0) {
+                if (imgBuffer[x + nY * width] == 255 && labelBuffer[x + nY * width] == -1) {
+                    stack[stackCount].x = x;
+                    stack[stackCount].y = nY;
+
+                    labelBuffer[x + nY * width] = labelNum;
+                    stackCount++;
+                }
+            }
+            if (pX < width) {
+                if (imgBuffer[pX + y * width] == 255 && labelBuffer[pX + y * width] == -1) {
+                    stack[stackCount].x = pX;
+                    stack[stackCount].y = y;
+
+                    labelBuffer[pX + y * width] = labelNum;
+                    stackCount++;
+                }
+            }
+            if (pY < height) {
+                if (imgBuffer[x + pY * width] == 255 && labelBuffer[x + pY * width] == -1) {
+                    stack[stackCount].x = x;
+                    stack[stackCount].y = pY;
+
+                    labelBuffer[x + pY * width] = labelNum;
+                    stackCount++;
+                }
+            }
+        }
     }
 
 }
--- openCVLibrary310/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary310/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: D:\\RedLaboratory\\Android\\AndroidStudioProjects\\LittleLaboratory\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: D:\\RedLaboratory\\AndroidStudio\\Workspace\\LittleLaboratory\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
