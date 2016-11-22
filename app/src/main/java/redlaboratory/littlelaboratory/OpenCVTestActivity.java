62835c8e7be7ef1c5d96de4967e90630676d0012
심지수 <rlj1202@gmail.com>
2016-11-23 00:58:49 +0900

    2016/11/22 @School
    이미 라벨링 함수가 OpenCV3 에 추가됨을 뒤늦게 알고 수정.
    역시 빠름. 13fps 정도? 좀만 더 하면 마커 검출 가능할듯!

--- app/src/main/java/redlaboratory/littlelaboratory/OpenCVTestActivity.java
+++ app/src/main/java/redlaboratory/littlelaboratory/OpenCVTestActivity.java
@@ -1,5 +1,6 @@
 package redlaboratory.littlelaboratory;
 
+import android.graphics.Region;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -19,6 +20,7 @@ import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.engine.OpenCVEngineInterface;
 import org.opencv.features2d.FeatureDetector;
 import org.opencv.imgproc.Imgproc;
@@ -107,139 +109,73 @@ public class OpenCVTestActivity extends AppCompatActivity implements CameraBridg
 
     }
 
-    private Mat grey;
-    private Mat rgba;
-
-    private Mat binarization;
-
-    private int width;
-    private int height;
-
-    private int minWidth;
-    private int maxWidth;
-    private int minHeight;
-    private int maxHeight;
-
-    private Rect[] rectsResult;
-
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        grey = inputFrame.gray();
-        rgba = inputFrame.rgba();
+        Log.v("LittleLaboratory", "OpenCV onCameraFrame");
 
-        width = rgba.width();
-        height = rgba.height();
+        Mat grey = inputFrame.gray();
+        Mat rgba = inputFrame.rgba();
 
-        minWidth = 10;
-        maxWidth = 1000;
-        minHeight = 10;
-        maxHeight = 1000;
+        int width = rgba.width();
+        int height = rgba.height();
 
-        binarization = new Mat();
+        Mat binarization = new Mat();
+        Mat binarizationInv = new Mat();
+
+        Mat labeled = new Mat();
+        Mat stats = new Mat();
+        Mat centroids = new Mat();
 
         Imgproc.threshold(grey, binarization, 127.0d, 225.0d, Imgproc.THRESH_BINARY_INV);
+        Imgproc.threshold(grey, binarizationInv, 127.0d, 225.0d, Imgproc.THRESH_BINARY);
+        int labels = Imgproc.connectedComponentsWithStats(binarization, labeled, stats, centroids, 4, CvType.CV_16U);
 
-        Log.v("LittleLaboratory", "onCameraFrame, " + inputFrame.rgba().type() + ", " + binarization.type());
+        Rect[] rects = new Rect[labels];
+        int rectsSize = 0;
 
-        {// labeling
-            int[] labelBuffer = new int[width * height];
-            for (int i = 0; i < width * height; i++) labelBuffer[i] = -1;
+        int[] rectInfo = new int[5];
+        double[] centroidInfo = new double[2];
 
-            int labelNum = 0;
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-//                    double curPixel = binarization.get(y, x)[0];
-//                    int curLabelNum = labelBuffer[x + y * width];
-//                    Log.v("LittleLaboratory", "im here1");
-//                    Point[] pointBufferA = new Point[width * height];
-//                    Point[] pointBufferB = new Point[width * height];
-//                    int arraySizeA = 0;
-//                    int arraySizeB = 0;
-//
-//                    if (curPixel == 255.0 && curLabelNum != -1) {// 화소가 있고 라벨링이 되어있지 않으면
-//                        labelNum++;// 초기값이 0이 었으니 1번부터 라벨링 번호를 매기도록 하겠음.
-//
-//                        pointBufferA[arraySizeA++] = new Point(x, y);// 버퍼에 시작될 포인트를 넣고
-//
-//                        while (true) {// 루프 돌린다
-//                            Log.v("LittleLaboratory", "im here2");
-//                            for (int i = 0; i < arraySizeA; i++) {
-//                                Point point = pointBufferA[i];
-//
-//                                if (binarization.get((int) point.y, (int) point.x)[0] == 255.0) {// 해당 포인트에 화소가 있으면
-//                                    labelBuffer[(int) (point.x + point.y * width)] = labelNum;// 라벨을 매기고
-//
-//                                    if (x - 1 > 0) pointBufferB[arraySizeB++] = new Point(point.x - 1, point.y);// B 버퍼에 오른, 왼, 위, 아래 포인트를 넣는다.
-//                                    if (x + 1 <= width) pointBufferB[arraySizeB++] = new Point(point.x + 1, point.y);
-//                                    if (y - 1 > 0) pointBufferB[arraySizeB++] = new Point(point.x, point.y - 1);
-//                                    if (y + 1 <= height) pointBufferB[arraySizeB++] = new Point(point.x, point.y + 1);
-//                                }
-//                            }Log.v("LittleLaboratory", "im here3");
-//
-//                            if (arraySizeB == 0) break;// A 버퍼에 화소를 가리키는 포인트가 하나도 없었다는 뜻 임으로 루프 종료.
-//
-//                            Point[] temp = pointBufferA;
-//                            pointBufferA = pointBufferB;// A는 이제 다 썼으니 B로 바꾸고
-//                            pointBufferB = temp;// A는 재활용을 위해 B 변수로
-//                            arraySizeB = 0;// 버퍼 사이즈 크기 값만을 줄임으로써 배열 객체 재활용!
-//
-//                            // B에 추가했던 포인트 배열을 A로 옮겼으니 A를 다시 루프.
-//                        }
-//                    }
-                }
+        for (int i = 1; i < stats.rows(); i++) {
+            stats.row(i).get(0, 0, rectInfo);
+            Rect rect = new Rect(rectInfo[0], rectInfo[1], rectInfo[2], rectInfo[3]);
+
+            if (rect.width / (float) width > 0.8 || rect.height / (float) height > 0.8) continue;
+            if (rect.area() < 200) continue;
+            int ratio = rect.height / rect.width;
+            if (5 < ratio || ratio < 0.2) continue;
+
+            rects[rectsSize++] = rect;
+
+            Mat cut = binarizationInv.submat(rect);
+
+            Mat labeledInv = new Mat();
+            Mat statsInv = new Mat();
+            Mat centroidsInv = new Mat();
+            Imgproc.connectedComponentsWithStats(cut, labeledInv, statsInv, centroidsInv, 4, CvType.CV_16U);
+            int[] rectInvInfo = new int[5];
+            for (int j = 1; j < statsInv.rows(); j++) {
+                statsInv.row(j).get(0, 0, rectInvInfo);
+                Rect rectInv = new Rect(rectInvInfo[0], rectInvInfo[1], rectInvInfo[2], rectInvInfo[3]);
+
+                if (rectInv.area() < 200) continue;
+                int ratioInv = rectInv.height / rectInv.width;
+                if (5 < ratioInv || ratioInv < 0.2) continue;
+
+                rectInv.x += rect.x;
+                rectInv.y += rect.y;
+
+                Imgproc.rectangle(rgba, rectInv.br(), rectInv.tl(), new Scalar(0, 255, 0));
             }
-            // 라벨링 완료.
 
+//            centroids.row(i).get(0, 0, centroidInfo);
+//            Point centroid = new Point(centroidInfo[0], centroidInfo[1]);
+
+            Imgproc.rectangle(rgba, rect.br(), rect.tl(), new Scalar(255, 0, 0));
+//            Imgproc.circle(rgba, centroid, 2, new Scalar(0, 255, 0));
         }
-//        {
-//            int[] imgBuffer = new int[width * height];
-//            int[] labelBuffer = new int[width * height];
-//
-//            rectsResult = new Rect[width * height];
-//
-//            for (int h = 0; h < height; h++) {
-//                for (int w = 0; w < width; w++) {
-//                    imgBuffer[w + h * width] = (int) binarization.get(h, w)[0];// get(y, x);
-//                    labelBuffer[w + h * width] = -1;
-//                }
-//            }
-//
-//            int labelCount = 0;
-//
-//            for (int h = 0; h < height; h++) {
-//                for (int w = 0; w < width; w++) {
-//                    if (imgBuffer[w + h * width] == 255 && labelBuffer[w + h * width] == -1) {
-//                        Rect temp = new Rect();
-//                        temp.x = w;
-//                        temp.y = h;
-//                        temp.width = 0;
-//                        temp.height = 0;
-//                        labelCount++;
-//
-//                        labeling(imgBuffer, labelBuffer, width, height, w, h, temp, labelCount);
-//
-//                        if (temp.width < minWidth || temp.width > maxWidth || temp.height < minHeight || temp.height > maxHeight) labelCount--;
-//                        else rectsResult[labelCount - 1] = temp;
-//                    }
-//                }
-//            }
-//        }
-//
-//        for (Rect rect : rectsResult) {
-//            if (rect == null) continue;
-//            Imgproc.rectangle(rgba, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(255, 0, 0));
-//        }
 
-//        Imgproc.threshold(mGrey, binarization, 100, 255, Imgproc.THRESH_BINARY);
-//        Imgproc.rectangle(mRgba, new Point(100, 200), new Point(100, 100), new Scalar(256, 0, 0), 3);
-
-//        ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-//        Mat intermediate = new Mat();
-//        Imgproc.Canny(mRgba, intermediate, 80, 100);
-//        Imgproc.findContours(intermediate, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0));
-//        Imgproc.drawContours(mRgba, contours, -1, new Scalar(255, 0, 0));
-
-        return binarization;
+        return rgba;
     }
 
     private void labeling(int[] imgBuffer, int[] labelBuffer, int width, int height, int x, int y, Rect label, int labelNum) {
