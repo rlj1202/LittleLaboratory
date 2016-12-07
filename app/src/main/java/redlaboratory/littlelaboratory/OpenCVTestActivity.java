16dbc8e4fc9bfcec5f5596af30fc8597075ed8ae
RedLaboratory <rlj1202@gmail.com>
2016-12-08 02:17:37 +0900

    2016/12/08 @Home
    이제야 어느정도 마커 검출 알고리즘에 대한 감이 생겼다. 일주일 안에 안정된 상태로 만들 수 있을까?

--- app/src/main/java/redlaboratory/littlelaboratory/OpenCVTestActivity.java
+++ app/src/main/java/redlaboratory/littlelaboratory/OpenCVTestActivity.java
@@ -17,6 +17,7 @@ import org.opencv.core.KeyPoint;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfKeyPoint;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
@@ -26,6 +27,7 @@ import org.opencv.features2d.FeatureDetector;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class OpenCVTestActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
@@ -111,6 +113,96 @@ public class OpenCVTestActivity extends AppCompatActivity implements CameraBridg
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return newMethod(inputFrame);
+    }
+
+    public Mat newMethod(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Mat grey = inputFrame.gray();
+        Mat rgba = inputFrame.rgba();
+
+        int width = rgba.width();
+        int height = rgba.height();
+
+        double screenArea = width * height;
+
+        Mat binarization = new Mat();
+        Mat binarizationInv = new Mat();
+
+        Imgproc.threshold(grey, binarization, 127, 255, Imgproc.THRESH_BINARY_INV);
+        Imgproc.threshold(grey, binarizationInv, 127, 255, Imgproc.THRESH_BINARY);
+
+        Mat labeled = new Mat();
+        Mat stats = new Mat();
+        Mat centroids = new Mat();
+
+        // int labels =
+        Imgproc.connectedComponentsWithStats(binarization, labeled, stats, centroids, 4, CvType.CV_16U);
+
+        int[] outterRectInfo = new int[5];
+        double[] outterRectCenterInfo = new double[2];
+        for (int outterRectIndex = 1; outterRectIndex < stats.rows(); outterRectIndex++) {
+            stats.row(outterRectIndex).get(0, 0, outterRectInfo);
+            centroids.row(outterRectIndex).get(0, 0, outterRectCenterInfo);
+            Rect outterRect = new Rect(outterRectInfo[0], outterRectInfo[1], outterRectInfo[2], outterRectInfo[3]);
+            Point outterRectCenter = new Point(outterRectCenterInfo[0], outterRectCenterInfo[1]);
+
+            if (outterRect.area() / screenArea >= 0.8) continue;
+            if (outterRect.area() / screenArea <= 0.0003) continue;
+            if (outterRect.area() < 100) continue;
+            if (outterRect.width / width > 0.8) continue;
+            if (outterRect.height / height > 0.8) continue;
+
+            Imgproc.rectangle(rgba, outterRect.br(), outterRect.tl(), new Scalar(255, 0, 0));// TODO
+
+            Mat binaryInvCut = binarizationInv.submat(outterRect);
+
+            Mat innerLabeled = new Mat();
+            Mat innerStats = new Mat();
+            Mat innerCentroids = new Mat();
+
+            Imgproc.connectedComponentsWithStats(binaryInvCut, innerLabeled, innerStats, innerCentroids, 4, CvType.CV_16U);
+
+            int passedInnerRects = 0;
+
+            int[] innerRectInfo = new int[5];
+            double[] innerRectCenterInfo = new double[2];
+            for (int innerRectIndex = 1; innerRectIndex < innerStats.rows(); innerRectIndex++) {
+                innerStats.row(innerRectIndex).get(0, 0, innerRectInfo);
+                innerCentroids.row(innerRectIndex).get(0, 0, innerRectCenterInfo);
+                Rect innerRect = new Rect(innerRectInfo[0], innerRectInfo[1], innerRectInfo[2], innerRectInfo[3]);
+                Point innerRectCenter = new Point(innerRectCenterInfo[0], innerRectCenterInfo[1]);
+
+                double areaRatio = innerRect.area() / outterRect.area();
+                final double areaRatioConstant = 0.03515625;
+
+                double centerDistanceSquare = Math.pow(innerRectCenter.x + outterRect.x - outterRectCenter.x, 2) + Math.pow(innerRectCenter.y + outterRect.y - outterRectCenter.y, 2);
+                double outterRectDistanceSquare = Math.pow(outterRect.tl().x - outterRect.br().x, 2) + Math.pow(outterRect.tl().y - outterRect.br().y, 2);
+                double distanceRatio = centerDistanceSquare / outterRectDistanceSquare;
+                final double distanceRatioConstant = 0.09765625;
+
+                boolean areaCondition = 0.01 <= areaRatio && areaRatio <= 0.05;
+
+                boolean distanceCondition = 0.04 <= distanceRatio && distanceRatio <= 0.15;
+
+                if (areaCondition && distanceCondition) {
+                    passedInnerRects++;
+
+                    Rect rectToDraw = innerRect.clone();
+                    rectToDraw.x += outterRect.x;
+                    rectToDraw.y += outterRect.y;
+                    Imgproc.rectangle(rgba, rectToDraw.br(), rectToDraw.tl(), new Scalar(0, 255, 0));
+                }
+            }
+
+            if (passedInnerRects > 0) {
+                Imgproc.rectangle(rgba, outterRect.br(), outterRect.tl(), new Scalar(255, 0, 0), 3);
+            }
+        }
+
+        return rgba;
+    }
+
+    public Mat old(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         Log.v("LittleLaboratory", "OpenCV onCameraFrame");
 
         Mat grey = inputFrame.gray();
@@ -140,42 +232,89 @@ public class OpenCVTestActivity extends AppCompatActivity implements CameraBridg
 
         for (int i = 1; i < stats.rows(); i++) {
             stats.row(i).get(0, 0, rectInfo);
+            centroids.row(i).get(0, 0, centroidInfo);
             Rect rect = new Rect(rectInfo[0], rectInfo[1], rectInfo[2], rectInfo[3]);
+            Point centroid = new Point(centroidInfo[0], centroidInfo[1]);
 
-//            if (rect.width / (float) width > 0.8 || rect.height / (float) height > 0.8) continue;
-            double areaRatio = rect.area() / screenArea;
-            if (areaRatio > 0.8 || areaRatio < 0.1) continue;
-            int ratio = rect.height / rect.width;
-//            if (5 < ratio || ratio < 0.2) continue;
+//            double areaRatio = rect.area() / screenArea;
+//            if (areaRatio > 0.8) continue;
+            if ((rect.height / (float) height) > 0.8) continue;
+            if ((rect.width / (float) width) > 0.8) continue;
 
             rects[rectsSize++] = rect;
 
-            Mat cut = binarizationInv.submat(rect);
+            Mat rgbaCut = rgba.submat(rect);
+            Mat binaryCut = binarizationInv.submat(rect);
 
             Mat labeledInv = new Mat();
             Mat statsInv = new Mat();
             Mat centroidsInv = new Mat();
-            Imgproc.connectedComponentsWithStats(cut, labeledInv, statsInv, centroidsInv, 4, CvType.CV_16U);
+            Imgproc.connectedComponentsWithStats(binaryCut, labeledInv, statsInv, centroidsInv, 4, CvType.CV_16U);
             int[] rectInvInfo = new int[5];
+            double[] centroidInvInfo = new double[2];
+            int checkedRectInvs = 0;
             for (int j = 1; j < statsInv.rows(); j++) {
                 statsInv.row(j).get(0, 0, rectInvInfo);
+                centroidsInv.row(j).get(0, 0, centroidInvInfo);
                 Rect rectInv = new Rect(rectInvInfo[0], rectInvInfo[1], rectInvInfo[2], rectInvInfo[3]);
+                Point centroidInv = new Point(centroidInvInfo[0], centroidInvInfo[1]);
 
-                if (rectInv.area() < 200) continue;
-                int ratioInv = rectInv.height / rectInv.width;
-                if (5 < ratioInv || ratioInv < 0.2) continue;
+                double centerDistanceSquare = Math.pow(centroidInv.x + rect.x - centroid.x, 2) + Math.pow(centroidInv.y + rect.y - centroid.y, 2);
+                double rectDistanceSquare = Math.pow(rect.tl().x - rect.br().x, 2) + Math.pow(rect.tl().y - rect.br().y, 2);
+                double distanceRatio = centerDistanceSquare / rectDistanceSquare;
+                if (distanceRatio > 0.005) continue;
+
+                double areaRatioInv = rectInv.area() / rect.area();
+                if (areaRatioInv > 0.8 || areaRatioInv < 0.05) continue;
+                if ((rectInv.height / (float) rect.height) > 0.8) continue;
+                if ((rectInv.width / (float) rect.width) > 0.8) continue;
+
+                checkedRectInvs++;
+
+                List<MatOfPoint> contours = new ArrayList<>();
+                Mat intermediate = new Mat();
+                Mat hierarchy = new Mat();
+                Imgproc.Canny(rgbaCut, intermediate, 80, 100);
+                Imgproc.findContours(intermediate, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+                for (Iterator<MatOfPoint> it = contours.iterator(); it.hasNext();) {
+                    MatOfPoint contour = it.next();
+
+                    Rect ret = null;
+
+                    MatOfPoint2f thisContour2f = new MatOfPoint2f();
+                    MatOfPoint approxContour = new MatOfPoint();
+                    MatOfPoint2f approxContour2f = new MatOfPoint2f();
+
+                    contour.convertTo(thisContour2f, CvType.CV_32FC2);
+
+                    Imgproc.approxPolyDP(thisContour2f, approxContour2f, 2, true);
+
+                    approxContour2f.convertTo(approxContour, CvType.CV_32S);
+
+                    if (approxContour.size().height == 4) {
+                        ret = Imgproc.boundingRect(approxContour);
+                    }
+
+                    if (ret == null) {
+                        it.remove();
+                    }
+                }
+                for (int index = 0; index < contours.size(); index++) Imgproc.drawContours(rgbaCut, contours, index, new Scalar(255, 255, 255), 2);
 
                 rectInv.x += rect.x;
                 rectInv.y += rect.y;
-
+                centroidInv.x += rect.x;
+                centroidInv.y += rect.y;
                 Imgproc.rectangle(rgba, rectInv.br(), rectInv.tl(), new Scalar(0, 255, 0));
+                Imgproc.circle(rgba, centroidInv, 2, new Scalar(0, 255, 0));
+                Imgproc.putText(rgba, "inner: area " + Math.round(areaRatioInv*10000)/10000.0 + ", distance " + Math.round(distanceRatio*10000)/10000.0 + ", contours " + contours.size(), new Point(centroidInvInfo[0] + rect.x, centroidInvInfo[1] + rect.y), 3, 1, new Scalar(255, 0, 255));
             }
 
-//            centroids.row(i).get(0, 0, centroidInfo);
-//            Point centroid = new Point(centroidInfo[0], centroidInfo[1]);
-
-            Imgproc.rectangle(rgba, rect.br(), rect.tl(), new Scalar(255, 0, 0));
-//            Imgproc.circle(rgba, centroid, 2, new Scalar(0, 255, 0));
+            if (checkedRectInvs != 0) {
+                Imgproc.rectangle(rgba, rect.br(), rect.tl(), new Scalar(255, 0, 0));
+                Imgproc.circle(rgba, centroid, 2, new Scalar(255, 0, 0));
+                Imgproc.putText(rgba, "outter", rect.br(), 3, 1, new Scalar(255, 0, 255));
+            }
         }
 
         return rgba;
